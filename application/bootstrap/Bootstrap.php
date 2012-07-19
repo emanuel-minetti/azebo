bc195ae7542b07ae4776733799cfedd65e062213
Emanuel Minetti <e.minetti@posteo.de>
2012-07-19 13:20:48 +0000

    Das Bootstrapping angepasst.

---
+++ application/bootstrap/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    public $frontController;
+    
+    protected function _initLocale() {
+        
+    }
+    
+    protected function _initViewSettings() {
+        
+    }
+
+
+}
+
