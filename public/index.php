83d4155485b9309dd9b28418bd838d1e484e5ba2
Emanuel Minetti <e.minetti@posteo.de>
2012-07-20 09:33:30 +0000

    Lizenz (GPL3) hinzugefügt.

--- public/index.php
+++ public/index.php
@@ -1,28 +1,22 @@
 <?php
-
-//// Define path to application directory
-//defined('APPLICATION_PATH')
-//    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-//
-//// Define application environment
-//defined('APPLICATION_ENV')
-//    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-//
-//// Ensure library/ is on include_path
-//set_include_path(implode(PATH_SEPARATOR, array(
-//    realpath(APPLICATION_PATH . '/../library'),
-//    get_include_path(),
-//)));
-//
-///** Zend_Application */
-//require_once 'Zend/Application.php';
-//
-//// Create application, bootstrap, and run
-//$application = new Zend_Application(
-//    APPLICATION_ENV,
-//    APPLICATION_PATH . '/configs/application.ini'
-//);
-//$application->bootstrap()
-//            ->run();
+/*
+ * 
+ *     This file is part of azebo.
+ * 
+ *     azebo is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     azebo is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with ADEmu.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *     Copyright 2012 Emanuel Minetti (e.minetti (at) arcor.de)
+ */
 
 require '../application/application.php';
