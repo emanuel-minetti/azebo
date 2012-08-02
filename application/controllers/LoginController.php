ed5746a3e987de0dd2cb7f92bb6fbfe7d3b1eb4a
Emanuel Minetti <e.minetti@posteo.de>
2012-08-02 00:23:56 +0000

    Den View-Helper 'Monatsliste' neu eingeführt und im UI 'Login' durch 'Anmelden' und 'Logout' durch 'Abmelden' ersetzt. TODO: Router einführen und die Links anpassen!

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -50,16 +50,16 @@ class LoginController extends Zend_Controller_Action {
         if ($request->isPost()) {
             if (!$form->isValid($request->getPost())) {
                 return $this->render('login');
-                $this->_logger->info("Login fehlgeschlagen: Validation gescheitert!");
+                $this->_logger->info("Anmeldung fehlgeschlagen: Validation gescheitert!");
             }
 
             if (!$this->_authService->authenticate($form->getValues())) {
                 $form->setDescription(
-                        'Login fehlgeschlagen! Bitte versuchen Sie es erneut!');
+                        'Anmeldung fehlgeschlagen! Bitte versuchen Sie es erneut!');
                 return $this->render('login');
-                $this->_logger->info("Login fehlgeschlagen: {$form->getValues()}");
+                $this->_logger->info("Anmeldung fehlgeschlagen: {$form->getValues()}");
             } else {
-                $this->_logger->info("Login erfolgreich: {$form->getValues()}");
+                $this->_logger->info("Anmeldung erfolgreich: {$form->getValues()}");
             }
 
             return $this->_helper->redirector->gotoSimple('index', 'index', 'default');
