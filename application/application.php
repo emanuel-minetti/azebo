616d86f8ce742e46b4f8f202341364c07c16185f
Emanuel Minetti <e.minetti@posteo.de>
2012-08-03 01:23:20 +0000

    Nur application.ini in app.ini umbenannt.

--- application/application.php
+++ application/application.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * 
  *     This file is part of azebo.
@@ -32,6 +31,6 @@ defined('APPLICATION_ENV')
 require_once 'Zend/Application.php';
 
 $application = new Zend_Application(APPLICATION_ENV,
-                APPLICATION_PATH . '/configs/application.ini');
+                APPLICATION_PATH . '/configs/app.ini');
 $application->bootstrap();
 $application->run();
