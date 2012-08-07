85641988b85c4e30e0e50d5c4bd3c26c3e7087cc
Emanuel Minetti <e.minetti@posteo.de>
2012-08-07 13:25:09 +0000

    Die Edit-Action dem Monats-Controller hinzugefügt. Die dazugehörige Route geschaffen, und die Links entsprechend gesetzt. Außerdem den Edit-View geschaffen und ihm zwei Tabellen hinzugefügt, die die übrigen Tage im Monat anzeigen und auch die entsprechenden Links zur Verfügung stellen.

--- application/controllers/MonatController.php
+++ application/controllers/MonatController.php
@@ -11,24 +11,28 @@ class MonatController extends AzeboLib_Controller_Abstract {
     }
 
     public function indexAction() {
-        
-        $monat = $this->_getParam('monat');
+
         $jahr = $this->_getParam('jahr');
-        
-        $this->view->monat = $monat;
+        $monat = $this->_getParam('monat');
+
         $this->view->jahr = $jahr;
-        
-        $feiertagsService = new Azebo_Service_Feiertag($jahr);        
-        $datum = new Zend_Date();
-        
+        $this->view->monat = $monat;
+
+        $feiertagsService = new Azebo_Service_Feiertag($jahr);
+
         // setze $datum auf den ersten des Monats
-        $datum->setDay(1);
-        $datum->setMonth($monat);
-        $datum->setYear($jahr);
+        $datum = new Zend_Date(array(
+                    'day' => 1,
+                    'month' => $monat,
+                    'year' => $jahr,
+                ));
         
+        //Finde die Anzahle der Tage in diesem Monat
+        $tageImMonat = $datum->get(Zend_Date::MONTH_DAYS);
+        $this->view->tageImMonat = $tageImMonat;
+
         // setze den Seitennamen
-        $this->erweitereSeitenName($datum->toString(' MMMM'));
-        $this->erweitereSeitenName($datum->toString(' yyyy'));
+        $this->erweitereSeitenName($datum->toString(' MMMM yyyy'));
 
         //aktiviere Dojo
         $this->view->dojo()->enable()
@@ -36,11 +40,11 @@ class MonatController extends AzeboLib_Controller_Abstract {
                 ->requireModule('dojox.grid.DataGrid')
                 ->requireModule('dojo.data.ItemFileReadStore')
                 ->requireModule('dojo._base.connect');
-        
+
         // befülle die Reihen
         $monatsDaten = new Zend_Dojo_Data();
         $monatsDaten->setIdentifier('datum');
-        for ($tag = 1; $tag <= $datum->get(Zend_Date::MONTH_DAYS) ; $tag++) {
+        for ($tag = 1; $tag <= $tageImMonat; $tag++) {
             $datum->setDay($tag);
             $feiertag = $feiertagsService->feiertag($datum);
             $monatsDaten->addItem(array(
@@ -48,9 +52,83 @@ class MonatController extends AzeboLib_Controller_Abstract {
                 'feiertag' => $feiertag['feiertag'],
             ));
         }
-        
+
         $this->view->monatsDaten = $monatsDaten;
     }
 
-}
+    public function editAction() {
+        $jahr = $this->_getParam('jahr');
+        $monat = $this->_getParam('monat');
+        $tag = $this->_getParam('tag');
 
+        $this->view->jahr = $jahr;
+        $this->view->monat = $monat;
+        $this->view->tag = $tag;
+
+        $feiertagsService = new Azebo_Service_Feiertag($jahr);
+        $zuBearbeitenderTag = new Zend_Date(array(
+                    'day' => $tag,
+                    'month' => $monat,
+                    'year' => $jahr,
+                ));
+        
+        $tageImMonat = $zuBearbeitenderTag->get(Zend_Date::MONTH_DAYS);
+        $this->view->tageImMonat = $tageImMonat;
+
+        // setze den Seitennamen
+        $this->erweitereSeitenName('-Bearbeite ');
+        $this->erweitereSeitenName($zuBearbeitenderTag->toString('d.M.yy'));
+
+        //aktiviere Dojo
+        $this->view->dojo()->enable()
+                ->setDjConfigOption('parseOnLoad', true)
+                ->requireModule('dojox.grid.DataGrid')
+                ->requireModule('dojo.data.ItemFileReadStore')
+                ->requireModule('dojo._base.connect');
+
+        if ($tag != 1) {
+            $datum = new Zend_Date(array(
+                        'day' => 1,
+                        'month' => $monat,
+                        'year' => $jahr,
+                    ));
+            $monatsDatenOben = new Zend_Dojo_Data();
+            $monatsDatenOben->setIdentifier('datum');
+            for ($tagIndex = 1; $tagIndex < $tag; $tagIndex++) {
+                $datum->setDay($tagIndex);
+                $feiertag = $feiertagsService->feiertag($datum);
+                $monatsDatenOben->addItem(array(
+                    'datum' => $feiertag['name'] . ' ' . $datum->toString('EE, dd.MM.YYYY'),
+                    'feiertag' => $feiertag['feiertag'],
+                ));
+            }
+        } else {
+            $monatsDatenOben = null;
+        }
+
+        if ($tag != $tageImMonat) {
+            // setze $datum auf den auf $tag folgenden Tag des Monats
+            $datum = new Zend_Date(array(
+                        'day' => $tag + 1,
+                        'month' => $monat,
+                        'year' => $jahr,
+                    ));
+            $monatsDatenUnten = new Zend_Dojo_Data();
+            $monatsDatenUnten->setIdentifier('datum');
+            for ($tagIndex = $tag + 1; $tagIndex <= $tageImMonat; $tagIndex++) {
+                $datum->setDay($tagIndex);
+                $feiertag = $feiertagsService->feiertag($datum);
+                $monatsDatenUnten->addItem(array(
+                    'datum' => $feiertag['name'] . ' ' . $datum->toString('EE, dd.MM.YYYY'),
+                    'feiertag' => $feiertag['feiertag'],
+                ));
+            }
+        } else {
+            $monatsDatenUnten = null;
+        }
+
+        $this->view->monatsDatenOben = $monatsDatenOben;
+        $this->view->monatsDatenUnten = $monatsDatenUnten;
+    }
+
+}
