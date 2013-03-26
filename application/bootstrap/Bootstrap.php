ad94fe11f7cc9a17f46218f954547d8134d70c47
Emanuel Minetti <e.minetti@posteo.de>
2013-03-26 09:24:57 +0000

    Versionsnummer auf 0.16 gesetzt.

--- application/bootstrap/Bootstrap.php
+++ application/bootstrap/Bootstrap.php
@@ -168,7 +168,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
                 addHelperPath('AzeboLib/View/Helper/', 'AzeboLib_View_Helper');
 
         //Die Version festlegen
-        $this->_view->version = '0.15';
+        $this->_view->version = '0.16';
     }
 
     protected function _initDbProfiler() {
