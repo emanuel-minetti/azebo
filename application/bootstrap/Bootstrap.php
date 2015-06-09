3a236ebdc7000c5ebf0bdd930131a531e9698ad7
root <root@localhost>
2015-06-09 16:55:09 +0000

    Versionsnummer auf 1.27 gesetzt.

--- application/bootstrap/Bootstrap.php
+++ application/bootstrap/Bootstrap.php
@@ -178,7 +178,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
                 addHelperPath('AzeboLib/View/Helper/', 'AzeboLib_View_Helper');
 
         //Die Version festlegen
-        $this->_view->version = '1.26';
+        $this->_view->version = '1.27';
     }
 
     protected function _initDbProfiler() {
