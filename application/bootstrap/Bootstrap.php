9fbd6d844dfc9e03427ffe6960fbcb6b75816837
Emanuel Minetti <e.minetti@posteo.de>
2016-02-02 13:05:41 +0000

    Die Versionsnummer und das Datum des Copyrights geändert.

--- application/bootstrap/Bootstrap.php
+++ application/bootstrap/Bootstrap.php
@@ -178,7 +178,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
                 addHelperPath('AzeboLib/View/Helper/', 'AzeboLib_View_Helper');
 
         //Die Version festlegen
-        $this->_view->version = '1.27';
+        $this->_view->version = '1.28';
     }
 
     protected function _initDbProfiler() {
