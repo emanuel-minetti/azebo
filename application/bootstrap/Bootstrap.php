db2218702a72ac00fea8e86108c30e6bd56c71ba
Emanuel Minetti <e.minetti@posteo.de>
2016-06-27 12:31:31 +0000

    Versionsnummer erhöht.

--- application/bootstrap/Bootstrap.php
+++ application/bootstrap/Bootstrap.php
@@ -197,7 +197,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
                 addHelperPath('AzeboLib/View/Helper/', 'AzeboLib_View_Helper');
 
         //Die Version festlegen
-        $this->_view->version = '1.37';
+        $this->_view->version = '1.38';
     }
 
     protected function _initDbProfiler() {
