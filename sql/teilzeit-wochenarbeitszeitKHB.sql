bc88d69b7b9cd488513164c7d09e16df096acf9f
Emanuel Minetti <e.minetti@posteo.de>
2017-11-27 15:27:46 +0000

    Die Abfragen reviewed und eine Merkwürdigkeit hinzugefügt.
    Die Merkwürdigkeit besteht darin, dass nicht alle Benutzernamen aus der
    allgemeinen Abfrage in den gefilterterten Abfrage auftauchen.

--- sql/teilzeit-wochenarbeitszeitKHB.sql
+++ sql/teilzeit-wochenarbeitszeitKHB.sql
@@ -1,7 +1,7 @@
 SELECT mitarbeiter_id,
   mitarbeiter.benutzername,
   kalenderwoche,
-  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`))),'%H:%i') AS soll_gesamt_in_sec
+  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`))),'%H:%i') AS soll_gesamt
 FROM arbeitsregel, mitarbeiter, Azebo_KHB_27_11_2017
 WHERE (soll IS NOT NULL
        AND soll != '00:00:00'
