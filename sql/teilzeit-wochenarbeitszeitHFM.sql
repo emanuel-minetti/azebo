48eda9234bbc13fee5672625f42cb5dbc9665ddd
Emanuel Minetti <e.minetti@posteo.de>
2018-01-03 13:57:11 +0000

    Rechenfehler

--- sql/teilzeit-wochenarbeitszeitHFM.sql
+++ sql/teilzeit-wochenarbeitszeitHFM.sql
@@ -2,8 +2,8 @@ SELECT mitarbeiter_id,
   mitarbeiter.benutzername,
   kalenderwoche,
   TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`))),'%H:%i') AS soll_gesamt,
-  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`)) * 1.0059829),'%H:%i') AS soll_neu,
-  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`)) * 0.0059829), '%H:%i')  AS soll_diff
+  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`)) * 1.01025641026),'%H:%i') AS soll_neu,
+  TIME_FORMAT(SEC_TO_TIME(SUM(TIME_TO_SEC( `soll`)) * 0.01025641026), '%H:%i')  AS soll_diff
 FROM arbeitsregel, mitarbeiter, Azebo_HFM_27_11_2017
 WHERE (soll IS NOT NULL
        AND soll != '00:00:00'
@@ -12,4 +12,3 @@ WHERE (soll IS NOT NULL
        AND mitarbeiter.id = mitarbeiter_id
        AND mitarbeiter.benutzername = Azebo_HFM_27_11_2017.benutzername)
 GROUP BY mitarbeiter_id, kalenderwoche;
-
